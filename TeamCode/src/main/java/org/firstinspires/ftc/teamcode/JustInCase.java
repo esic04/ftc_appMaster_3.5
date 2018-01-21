8f1d5598b7461d117937e674a8c6490b7cebe719
Ethan <ethan_chang@icloud.com>
2018-01-20 17:59:29 -0800

    finished programming just in case

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JustInCase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JustInCase.java
@@ -25,6 +25,69 @@ public class JustInCase extends LinearOpMode {
 
     @Override
     public void runOpMode(){
+        left = hardwareMap.get(DcMotor.class, "left");
+        right = hardwareMap.get(DcMotor.class, "right");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        armUp = hardwareMap.get(DcMotor.class, "armUp");
+        servo = hardwareMap.get(Servo.class, "servo");
+        servo2 = hardwareMap.get(Servo.class, "servo2");
+        servoJewel = hardwareMap.get(Servo.class, "servoJewel");
 
+        servoClosePos = 0.15;
+        servo2ClosePos = 0.85;
+        servoOpenPos = 0.5;
+        servo2OpenPos = 0.5;
+
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armUp.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        sleep(500);
+
+        left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armUp.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        servo.setPosition(servoClosePos);
+        servo2.setPosition(servo2ClosePos);
+        servoJewel.setPosition(1);
+
+        telemetry.addData("status", "initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        left.setPower(0.2);
+        right.setPower(0.2);
+        frontLeft.setPower(0.2);
+        frontRight.setPower(0.2);
+        armUp.setPower(0.4);
+
+        armUp.setTargetPosition(armUp.getCurrentPosition() - 1000);
+
+        while(opModeIsActive() && armUp.isBusy()){
+            telemetry.addData("arm up position", armUp.getCurrentPosition());
+            telemetry.update();
+        }
+
+        left.setTargetPosition(left.getCurrentPosition() - 1701);
+        right.setTargetPosition(right.getCurrentPosition() + 1701);
+        frontLeft.setTargetPosition(frontLeft.getCurrentPosition() + 1997);
+        frontRight.setTargetPosition(frontRight.getCurrentPosition() - 1997);
+
+        while (opModeIsActive() && left.isBusy() && right.isBusy() && frontLeft.isBusy() && frontRight.isBusy()) {
+            telemetry.addData("back right current position", right.getCurrentPosition());
+            telemetry.addData("back left current position", left.getCurrentPosition());
+            telemetry.addData("front right current position", frontRight.getCurrentPosition());
+            telemetry.addData("front left current position", frontLeft.getCurrentPosition());
+            telemetry.update();
+        }
+
+        requestOpModeStop();
     }
 }
